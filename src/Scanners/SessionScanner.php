d1232483ad5e5512a31362f2339bfe26893f74d2
Henrique Mayer <hmayer@gmail.com>
2025-05-29 02:27:29 -0300

    Add `SessionScanner` to validate SESSION_DRIVER configuration for production readiness

---
+++ src/Scanners/SessionScanner.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Elohim\LaravelSecurityCheck\Scanners;
+
+use Illuminate\Support\Env;
+
+final class SessionScanner extends AbstractScanner implements ScannerInterface
+{
+    public static string $description = "Session Scanner";
+
+    public function execute(): void
+    {
+        $this->info(self::CHECK . __("Verifying Session..."));
+
+        $sessionDriver = Env::get('SESSION_DRIVER', 'file');
+
+        if (preg_match('/^(file|array)/m', $sessionDriver)) {
+            $this->warn(self::WARNING . __("SESSION_DRIVER not recommended for production: :driver", ['driver' => $sessionDriver]));
+        }
+        if (preg_match('/^(redis|database|cookie)/m', $sessionDriver)) {
+            $this->info(self::CHECK . __("SESSION_DRIVER well configured: :driver", ['driver' => $sessionDriver]));
+        }
+    }
+}
